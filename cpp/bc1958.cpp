ea72729f78a9846e66a54fbd2726882513bd84a3
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2023-12-02 22:14:16 -0300

    Commit 02/12/2023

---
+++ cpp/bc1958.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double num;
+	
+	cin >> num;
+	
+	cout << setprecision(4) << scientific <<  uppercase << showpos <<num << endl;
+}
