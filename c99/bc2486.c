d8000af2b34e782e3bef8d632ed7207acd499945
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-05-05 23:09:07 -0300

    Commit 05/05/2024

---
+++ c99/bc2486.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//#include <stdbool.h>
+int main()
+{
+	int qLi, qtA, sTo, i;
+	char line[50];
+	char nAl[20];
+	
+	while(1)
+	{
+		strcpy(nAl, "");
+		sTo = 0;
+		
+		scanf("%d", &qLi);
+		getchar();
+		
+		if(qLi == 0) break;
+		else
+		{
+			for(i = 0; i < qLi; i++)
+			{
+				
+				fgets(line, sizeof(line), stdin);
+				
+				char *token = strtok(line, " \n");
+				
+				qtA = atoi(token);
+				token = strtok(NULL, " \n");
+				
+				while(token != NULL)
+				{
+					strcat(nAl, token);
+					strcat(nAl, " ");
+					token = strtok(NULL, " \n");
+				}
+				
+				
+			}
+		}
+	}
+}
