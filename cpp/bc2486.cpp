21243d43f2ebcc801151cea6d3e7feeeadf7b67a
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-05-28 23:36:50 -0300

    Commit 28/05/2024

--- cpp/bc2486.cpp
+++ cpp/bc2486.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <sstream>
-
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	string line, token;
-	int qtdL, sTo;
+	string line, token, nAl;
+	int qtdL, sTo, qtA, tam;
+	vector <string> vec;
 	
 	while(true)
 	{
 		sTo = 0;
 		
-		cin >> qtdLi;
+		cin >> qtdL;
 		cin.ignore();
 		
-		if(qLi == 0) break;
+		if(qtdL == 0) break;
 		else
 		{
-			getline(cin, line);
-			
-			issstream iss(line);
-			
-			
+		    for(int i = 0; i < qtdL; i++)
+    		{    
+    			getline(cin, line);
+    			
+    			istringstream iss(line);
+    			
+    			while(iss >> token)
+    				vec.push_back(token);
+    			
+    			qtA = stoi(vec[0]);
+    			tam = vec.size();
+    			
+    			for(int j = 1; j < tam; j++)
+    			{
+    			    nAl += vec[j];
+    			    nAl += " ";
+    			}
+    			
+    			cout << qtA <<" " << nAl << endl;
+    			
+    			vec.clear();
+    			nAl.clear();
+		    }
 		}
 	}
 	
