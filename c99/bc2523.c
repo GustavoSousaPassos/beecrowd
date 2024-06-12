bde21362359316a8efe0902172c82077a13b6d01
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-06-11 23:58:31 -0300

    Commit 11/06/2024

--- c99/bc2523.c
+++ c99/bc2523.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdbool.h>
 int main()
 {
+	char cLe[26], token;
 	
+	while(true)
+	{
+		cLe[0] = '\0';
+		fgets(cLe, sizeof(cLe), stdin);
+		
+		if(cLe == EOF) break;
+		
+	}
 }
