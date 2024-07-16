0486198fa2c2b6b1e974ec85df3b75708b756de7
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-07-15 23:52:17 -0300

    Commit 15/07/2024

--- c99/bc2533.c
+++ c99/bc2533.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 int main()
 {
-	int fSo, sSo, fVal, sVal, nCa;
+	int fSo, sSo, fVal, sVal, nCa, i, j;
+	char str[4];
 	
 	while(scanf("%d", &nCa) == 1)
-	{
-		
+	{	
+		for(i = 0; i < nCa; i++)
+		{	
+		    getchar();
+			fgets(str, sizeof(str), stdin);
+			
+			char *token = strtok(str, " \n");
+			
+			fVal = atoi(token);
+			
+			token = strtok(NULL, " \n");
+			
+			sVal = atoi(token);
+			
+			printf("%d %d\n", fVal, sVal);
+			
+		}
 	}
 }
