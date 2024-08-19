6b1b5cc2f635e13f7959d7f229ea004dd4ef6039
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-08-18 23:51:24 -0300

    Commit 18/08/2024

---
+++ python/bc3174.py
@@ -0,0 +1,16 @@
+nPre = 0
+nCa = int(input())
+
+for c in range(0, nCa):
+    cVal = input().split(" ")
+
+    nSe = cVal[1]
+    qtdH = int(cVal[2])
+
+    if nSe == "bonecos":
+        nPre += int(qtdH / 8)
+    elif nSe == "arquitetos": nPre += int(qtdH / 4)
+    elif nSe == "musicos": nPre += int(qtdH / 6)
+    elif nSe == "desenhistas": nPre += int(qtdH / 12)
+
+print(nPre)
