c35f842a7096be348fcb8f7e32d3c9dc906a54a4
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-08-29 23:52:51 -0300

    Second Commit 29/08/2024

--- c99/bc2552.c
+++ c99/bc2552.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 int main()
 {
-	int nLi, nCol, i, j;
+	int nLi, nCol, i, j, sum;
 	char str[50];
 	bool fVe, sVe, tVe, qVe;
 	
@@ -25,18 +25,42 @@ int main()
 			}
 		}
 		
-		for(i = 0; i < nLi; i++)
+		for(i = 0; i < nCol; i++)
 		{
-			int sum = 0;
+			sum = 0;
+			sVe = mat[1][i] == 1;
+			tVe = mat[0][i-1] == 1;
+			qVe = mat[0][i+1] == 1;
+			
+			if(mat[0][i] == 1) mat[0][i] = 9;
+			else
+			{
+				if(sVe) sum++;
+				if(tVe) sum++;
+				if(qVe) sum++;
+				
+				mat[0][i] = sum;
+			}
+		}
+		
+		for(i = 1; i < nLi; i++)
+		{
+			sum = 0;
+			
 			for(j = 0; j < nCol; j++)
 			{
+				fVe = mat[i-1][j] == 1;
+				sVe = mat[i+1][j] == 1;
+				tVe = mat[i][j-1] == 1;
+				qVe = mat[i][j+1] == 1;
+				
 				if(mat[i][j] == 1) mat[i][j] = 9;
 				else
 				{
-					if(mat[i-1][j] == 1) sum++;
-					if(mat[i+1][j] == 1) sum++;
-					if(mat[i][j-1] == 1) sum++;
-					if(mat[i][j+1] == 1) sum++;
+					if(fVe) sum++;
+					if(sVe) sum++;
+					if(tVe) sum++;
+					if(qVe) sum++;
 					
 					mat[i][j] = sum;
 				}
