d708d4a8b18c14e2f163b91e895deb4b9dd582b3
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-11-16 11:58:56 -0300

    Commit 16/11/2024

--- python/bc2626.py
+++ python/bc2626.py
@@ -1,6 +1,11 @@
 while True:
     try:
-        msg = ["Banana", "sla", "oooo"]
+        msg = {
+            0: "Putz vei, o Leo ta demorando muito pra jogar...",
+            1: "Os atributos dos monstros vao ser inteligencia, sabedoria...",
+            2: "Iron Maiden's gonna get you, no matter how far!",
+            3: "Urano perdeu algo muito precioso..."
+        }
         res = -1
         line = input().split(" ")
 
@@ -9,6 +14,11 @@ while True:
         pepper = line[2]
 
         if dodo == leo and leo == pepper: res = 0
+        else:
+            if (dodo == "papel" and leo == "pedra" and pepper == "pedra") or \
+                (dodo == "tesoura" and leo == "papel" and pepper == "papel") or \
+                (dodo == "pedra" and leo == "tesoura" and pepper == "tesoura"):  res = 1
+            elif (leo == "papel" and dodo == "pedra" and pepper == "pedra"): res = 2
 
         print(f"{msg[res]}")
     except EOFError:
