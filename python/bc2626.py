9e7496011cf9bf579aac15a5346997b34b6707fd
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-11-18 11:54:53 -0300

    Commit 18/11/2024

--- python/bc2626.py
+++ python/bc2626.py
@@ -16,9 +16,14 @@ while True:
         if dodo == leo and leo == pepper: res = 0
         else:
             if (dodo == "papel" and leo == "pedra" and pepper == "pedra") or \
-                (dodo == "tesoura" and leo == "papel" and pepper == "papel") or \
-                (dodo == "pedra" and leo == "tesoura" and pepper == "tesoura"):  res = 1
-            elif (leo == "papel" and dodo == "pedra" and pepper == "pedra"): res = 2
+               (dodo == "tesoura" and leo == "papel" and pepper == "papel") or \
+               (dodo == "pedra" and leo == "tesoura" and pepper == "tesoura"):  res = 1
+            elif (leo == "papel" and dodo == "pedra" and pepper == "pedra") or \
+                 (leo == "tesoura" and dodo == "papel" and pepper == "papel") or \
+                 (leo == "pedra" and dodo == "tesoura" and pepper == "tesoura"): res = 2
+            elif (pepper == "papel" and leo == "pedra" and dodo == "pedra") or \
+                 (pepper == "tesoura" and leo == "papel" and dodo == "papel") or \
+                 (pepper == "pedra" and leo == "tesoura" and dodo == "tesoura"): res = 3
 
         print(f"{msg[res]}")
     except EOFError:
