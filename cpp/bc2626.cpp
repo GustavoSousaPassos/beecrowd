391b3fc248202b2a350b8a9afa071bd9798406f7
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2024-11-21 22:08:30 -0300

    Commit 21/11/2024

--- cpp/bc2626.cpp
+++ cpp/bc2626.cpp
@@ -23,6 +23,19 @@ int main()
 		res = -1;
 		
 		if(dodo == leo && leo == pepper) res = 0;
+		else
+		{
+			if((dodo == "tesoura" && leo == "papel" && pepper == "papel") ||
+			(dodo == "pedra" && leo == "tesoura" && pepper == "tesoura") ||
+			(dodo == "papel" && leo == "pedra" && pepper == "pedra")) res = 1;
+			else if((leo == "tesoura" && dodo == "papel" && pepper == "papel") ||
+			(leo == "pedra" && dodo == "tesoura" && pepper == "tesoura") ||
+			(leo == "papel" && dodo == "pedra" && pepper == "pedra")) res = 2;
+			else if((pepper == "tesoura" && leo == "papel" && dodo == "papel") ||
+			(pepper == "pedra" && leo == "tesoura" && dodo == "tesoura") ||
+			(pepper == "papel" && leo == "pedra" && dodo == "pedra")) res = 3;
+			else res = 0;
+		}
 		
 		switch(res)
 		{
