88ed10eb02de48b8b8782e61ccc2af119bbd0c5a
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-02-17 21:29:25 -0300

    Commit 17/02/2025

--- c99/bc2686.c
+++ c99/bc2686.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 int main()
 {
 	double an, h, m, s;
+	char res[20];
 	
-	while(scanf("%lf", &an) == 1)
+	while(1)
 	{
-		h = an / 15;
-		m = fmod(an ,15) * 4;
-		s = round(m * 60);
-		
-		
-		printf("%02.0f:%02.0f:%02.0f\n", h, m, s);
+		if(scanf("%lf", &an) == 1)
+		{
+			h = an / 15;
+			m = fmod(an ,15) * 4;
+			s = round((m - floor(m)) * 60);
+			
+			if(an < 90 || an == 360) strcpy(res, "Bom Dia!!");
+			else if(an < 180) strcpy(res, "Boa Tarde!!");
+			else if(an < 270) strcpy(res, "Boa Noite!!");
+			else strcpy(res, "De Madrugada!!");
+			
+			h = fmod(h + 6, 24);
+			puts(res);
+			printf("%02.0f:%02.0f:%02.0f\n", h, m, s);
+		}
+		else break;
 	}
 }
