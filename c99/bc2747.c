8e3047176847edd2a2425b9175f108d804df2c6d
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-09-20 22:36:11 -0300

    Resolução do exercício 2747

---
+++ c99/bc2747.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0; i < 7;i++)
+        i == 0 || i == 6? printf("---------------------------------------\n") : printf("|                                     |\n");
+}
+ 
+
---
+++ cpp/bc2747.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i < 7;i++)
+    {
+        if(i == 0 || i == 6) cout << "---------------------------------------" << endl;
+        else cout <<  "|                                     |" << endl;
+    }
+}
+ 
+
