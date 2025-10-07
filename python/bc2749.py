466ab562ac397e1a67889a7409aa2c01fab8a1e1
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-10-07 14:22:04 -0300

    Resolução do exercício 2749 na linguagem python

---
+++ python/bc2749.py
@@ -0,0 +1,10 @@
+fLi = "---------------------------------------"
+sLi = "|                                     |"
+
+print(fLi)
+print("|x = 35                               |")
+print(sLi)
+print("|               x = 35                |")
+print(sLi)
+print("|                               x = 35|")
+print(fLi)
