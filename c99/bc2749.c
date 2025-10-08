4a58353bd739eec647619207113eaded501bf558
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-10-08 14:28:10 -0300

    Resolução do exercício 2749 nas linguagens c e c++

---
+++ c99/bc2749.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+int main()
+{
+	char fLi[] = "---------------------------------------";
+	char sLi[] = "|                                     |";
+	
+	puts(fLi);
+	puts("|x = 35                               |");
+	puts(sLi);
+	puts("|               x = 35                |");
+	puts(sLi);
+	puts("|                               x = 35|");
+	puts(fLi);
+}
+
+
--- cpp/bc2749.cpp
+++ cpp/bc2749.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	int pEs = 0, pDi = 32;
-    
-    for(int i = 0; i < 7; i++)
-    {
-    	if(i == 0 || i == 6) cout << "---------------------------------------" << endl;
-    	else
-    	{
-	        if(i % 2 != 0) 
-	        {
-	            cout << "|" << setw(pEs) << "x = 35" << fixed;
-				cout << setw(pDi) << "|"  << fixed << endl;
-	            pEs += 16;
-	            pDi -= 17;
-	        }
-	        else cout << "|                                     |" << endl;
-	 	}
-    }
-    
-    return 0;
+	string fLi = "---------------------------------------";
+	string sLi = "|                                     |";
+	
+	cout << fLi << endl;
+	cout << "|x = 35                               |" << endl;
+	cout << sLi << endl;
+	cout << "|               x = 35                |" << endl;
+	cout << sLi << endl;
+	cout << "|                               x = 35|" << endl;
+	cout << fLi << endl;
 }
