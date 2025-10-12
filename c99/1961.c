9270bb9164006ddc5204a832ba7258fc4eaedc0a
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-10-12 20:23:08 -0300

    Resolução do exercício 1961 na linguagem c

---
+++ c99/1961.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+char* calcRe(int pu, int qtd)
+{
+	int i;
+	int * cVal = malloc(sizeof(int)* qtd);
+	char * sCa = malloc(sizeof(char)*(qtd*2));
+	
+	fgets(sCa, qtd*2, stdin);
+	
+	char* token = strtok(sCa, " \n");
+	
+	for(i = 0; i < qtd; i++)
+	{
+	    cVal[i] = atoi(token);
+	    token = strtok(NULL, " \n");
+	}
+	
+	for(i = 0; i < qtd-1; i++)
+	{
+	    if(abs(cVal[i] - cVal[i+1]) > pu)
+	    {
+	        free(cVal);
+	        free(sCa);
+	        return "GAME OVER";
+	    }
+	}
+	
+	free(cVal);
+	free(sCa);
+	return "YOU WIN";
+}
+
+
+int main()
+{
+	int aPu, qCa;
+	
+	scanf("%d %d", &aPu, &qCa);
+	getchar();
+	
+	puts(calcRe(aPu, qCa));
+}
