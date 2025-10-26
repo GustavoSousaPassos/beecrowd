ce9cf234da5ca6d76a8a204d900855aa2ff768e7
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-10-26 20:00:31 -0300

    Resolução do exercício 2754 na linguagem pyhton

---
+++ python/bc2754.py
@@ -0,0 +1,12 @@
+fVa = 234.345
+sVa = 45.698
+
+print(f"{fVa:.6f} - {sVa:.6f}")
+print(f"{fVa:.0f} - {sVa:.0f}")
+print(f"{fVa:.1f} - {sVa:.1f}")
+print(f"{str(fVa)[:6]} - {sVa:.2f}")
+print(f"{fVa:.3f} - {sVa:.3f}")
+print(f"{fVa:e} - {sVa:e}")
+print(f"{fVa:E} - {sVa:E}")
+print(f"{fVa:.3f} - {sVa:.3f}")
+print(f"{fVa:.3f} - {sVa:.3f}")
