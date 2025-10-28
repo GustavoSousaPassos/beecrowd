d96c506d5e3b3db263a75761ccc62ee6f791798a
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2025-10-28 16:42:52 -0300

    Resolução do exercício 2755 nas linguagens c, c++ e python

---
+++ c99/bc2755.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	char res[] = "\"Ro'b'er\tto\\/\"\n(._.) ( l: ) ( .-. ) ( :l ) (._.)\n(^_-) (-_-) (-_^)\n(\"_\") ('.')";
+	puts(res);
+}
---
+++ cpp/bc2755.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() 
+{
+    string res = "\"Ro'b'er\tto\\/\"\n(._.) ( l: ) ( .-. ) ( :l ) (._.)\n(^_-) (-_-) (-_^)\n(\"_\") ('.')";
+    cout << res << endl;
+    return 0;
+}
